a5e1f7fca18ebcbda4d8c70f4638d1fd06b97cab
Sreedeep Ghosh <155952425+Sreed-eep@users.noreply.github.com>
2024-06-01 16:58:46 +0530

    Add files via upload

---
+++ MusicEvent.js
@@ -0,0 +1,8 @@
+function buyTickets() {
+    alert("Ticket purchase feature is coming soon!");
+}
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Your message has been sent! We will get back to you soon.");
+});
